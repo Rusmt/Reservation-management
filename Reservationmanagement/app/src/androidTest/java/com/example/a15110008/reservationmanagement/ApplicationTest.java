d6e75a16d87bde754fa3320d7076988954c84840
Rusmt <r.kunitomi8@gmail.com>
2016-08-24 11:40:38 +0900

    initialcommit

---
+++ Reservationmanagement/app/src/androidTest/java/com/example/a15110008/reservationmanagement/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.a15110008.reservationmanagement;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/Availability.java
@@ -0,0 +1,13 @@
+package com.example.a15110008.reservationmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Availability extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_availability);
+    }
+}
