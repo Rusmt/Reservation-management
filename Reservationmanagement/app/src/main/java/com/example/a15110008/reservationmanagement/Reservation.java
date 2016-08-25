fb640c911980248b40c46c7a089cda33d22e06f1
Rusmt <r.kunitomi8@gmail.com>
2016-08-25 10:41:00 +0900

    Reservation

---
+++ Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/Reservation.java
@@ -0,0 +1,18 @@
+package com.example.a15110008.reservationmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Reservation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+
+    }
+
+}
