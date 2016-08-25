09780822d035a62278586e8dd218769a3ec671d0
utida <ahiru.fhd7h@gmail.com>
2016-08-25 10:31:04 +0900

    フロア選択画面と部屋番号選択画面の作成（途中）

---
+++ Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/RoomActivity.java
@@ -0,0 +1,15 @@
+package com.example.a15110008.reservationmanagement;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class RoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room);
+
+    }
+}
