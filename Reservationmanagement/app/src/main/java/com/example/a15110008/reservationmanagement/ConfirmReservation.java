9ccba4f185a42be1c8cb418d02bc243cf3a8fbf0
16DELL-52\15110014 <coconatto0703@gmail.com>
2016-09-01 15:55:38 +0900

    RoomInfoを予約情報を格納するクラスに変更

--- Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/ConfirmReservation.java
+++ Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/ConfirmReservation.java
@@ -1,35 +1,88 @@
 package com.example.a15110008.reservationmanagement;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.StringDef;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TabHost;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
 public class ConfirmReservation extends AppCompatActivity {
+
+    public static Intent getStartActivityIntent(Context context) {
+        Intent intent = new Intent(context, ConfirmReservation.class);
+        return intent;
+    }
+
+    private static final String TAG = ConfirmReservation.class.getSimpleName();
+
+    //FIreBaseのURL
+    private final static String BASE_URL = "https://reservationmanagement-161c6.firebaseio.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_reservation);
 
+        //firebase設定
+        Firebase.setAndroidContext(getApplicationContext());
 
         //利用時間の取得
         TextView srselectedhour =(TextView)findViewById((R.id.selectedHour));
         srselectedhour.setText(getIntent().getExtras().getString("selectedhour"));
+        final RoomList reserveInfo = (RoomList)getIntent().getSerializableExtra("reserve_info");
 
-
-        //確定ボタンを御した時の処理
         Button crOKBtn = (Button)findViewById(R.id.crOKButton);
 
         crOKBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void  onClick(View v){
                 Intent intent = new Intent(ConfirmReservation.this, Reservation.class);
+                addresetrvation(reserveInfo);
                 startActivity(intent);
             }
         });
     }
+
+
+    //予約データをFireBaseに入力するメソッド
+    private void addresetrvation(RoomList reserveinfo){
+
+        String Date = reserveinfo.getSelectedYear() + reserveinfo.getSelectedMonth() + reserveinfo.getSelectedDay();
+        String Floor = reserveinfo.getSelectedFloor();
+        String size = reserveinfo.getSelectedSize();
+        String StartTime = ;
+        String endTime = ;
+
+
+        //予約データ
+        Firebase messages = new Firebase(BASE_URL + "");//値を設定するDBを決める
+        messages.push().setValue(
+
+
+                new ReservationInfo(),//設定する項目数
+                new Firebase().CompletionListener(){
+
+            @Override
+                    public void onComplete(FirebaseError firebaseError, Firebase firebase){
+                if (firebaseError != null) {
+                    Log.e(TAG, firebaseError.getMessage(), firebaseError.toException());
+                    return;
+                }
+
+                Log.d(TAG, "data successfully saved!");
+            }
+        }
+
+        );
+
+    }
+
+
 }
--- Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/RoomList.java
+++ Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/RoomList.java
@@ -1,12 +1,72 @@
 package com.example.a15110008.reservationmanagement;
 
+import android.util.SparseBooleanArray;
+
 import java.io.Serializable;
 
 public class RoomList implements Serializable {
+    //部屋の予約情報を収集するクラス
 
-    public String[][] rooms;
+    int selectedYear = 0;//年
+    int selectedMonth = 0;//月
+    int selectedDay = 0;//日
+    String selectedFloor = "";//階層
+    String selectedSize = "";//部屋の大きさ
+    SparseBooleanArray checked;//選択利用時間
 
-    public RoomList(String[][] roomList){
-        this.rooms = roomList;
+    //setter
+
+    public void ReserveYear(int  year){
+        this.selectedYear = year;
     }
+
+    public void ReserveMonth(int  month){
+        this.selectedMonth = month;
+    }
+
+    public void ReserveDay(int  day){
+        this.selectedDay = day;
+    }
+
+    public void ReserveFloor(String floor){
+        this.selectedFloor = floor;
+    }
+
+    public void ReserveSize(String size){
+        this.selectedSize = size;
+    }
+
+    public void ReserveHour(SparseBooleanArray checked){
+        this.checked = checked;
+    }
+
+    //getter
+
+    public String getSelectedYear(){
+        String year = Integer.toString(selectedYear);
+        return year;
+    }
+
+    public String getSelectedMonth(){
+        String month = Integer.toString(selectedMonth);
+        return month;
+    }
+
+    public String getSelectedDay(){
+        String day = Integer.toString(selectedDay);
+        return day;
+    }
+
+    public String getSelectedFloor(){
+        return selectedFloor;
+    }
+
+    public String getSelectedSize(){
+        return selectedSize;
+    }
+
+    public  String getchecked(){
+        return checked;
+    }
+
 }
