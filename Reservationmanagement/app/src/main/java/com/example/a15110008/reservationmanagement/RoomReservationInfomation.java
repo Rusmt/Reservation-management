d4efc0b4489487af1cb5e123da87163193561a52
16DELL-52\15110014 <coconatto0703@gmail.com>
2016-09-02 13:00:24 +0900

    発表用更新

--- Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/RoomReservationInfomation.java
+++ Reservationmanagement/app/src/main/java/com/example/a15110008/reservationmanagement/RoomReservationInfomation.java
@@ -16,7 +16,6 @@ public class RoomReservationInfomation implements Serializable {
     String selectedFloor = "1F";//階層
     String selectedSize = "大";//部屋の大きさ
     String selectedTime = "";//利用時間
-    SparseBooleanArray checked;//選択利用時間
 
     //setter
 
@@ -40,9 +39,7 @@ public class RoomReservationInfomation implements Serializable {
         this.selectedSize = size;
     }
 
-    public void ReserveHour(SparseBooleanArray checked){
-        this.checked = checked;
-    }
+    //public void ReserveHour(SparseBooleanArray checked){this.checked = checked;}
 
     public void ReserveTime(String time){
         this.selectedTime = time;
